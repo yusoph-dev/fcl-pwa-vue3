dd7ea634a49f0ab33a85250f8f4346e246183dd9
ren <alcren12@gmail.com>
2022-06-03 15:41:22 +0800

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div>
+      <button @click="connect">
+        {{ userData ? "Logout" : "Login" }}
+      </button>
+      <div>USER ADDRESS : {{ userData?.addr }}</div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+<script>
+import { defineComponent, ref } from "vue";
+import * as fcl from "@onflow/fcl";
 
 export default defineComponent({
   name: "HomeView",
-  components: {
-    HelloWorld,
+  setup() {
+    const userData = ref(null);
+    fcl
+      .config()
+      .put("accessNode.api", "https://access-testnet.onflow.org")
+      .put(
+        "discovery.wallet",
+        "https://staging.accounts.meetdapper.com/fcl/authn-restricted"
+      )
+      .put("discovery.wallet.method", "POP/RPC");
+
+    const connect = async () => {
+      if (userData.value !== null) {
+        await fcl.unauthenticate();
+        userData.value = null;
+      } else {
+        await fcl.authenticate();
+        userData.value = await fcl.currentUser().snapshot();
+      }
+    };
+
+    return {
+      connect,
+      userData,
+    };
   },
 });
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  pwa: {
+    name: 'FCL TEST',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
   transpileDependencies: true,
 });
