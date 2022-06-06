f50de92c3da95469de1a1c991371ec814267d293
damascus08 <yusophh@gmail.com>
2022-06-06 17:33:51 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <router-view />
+  <div>
+    <router-view />
+    <div class="download" v-if="shown">
+      <span @click="dismissPrompt" class="exit">x</span>
+      <span @click="installPWA">Install App</span>
+    </div>
+  </div>
 </template>
-
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    shown: false,
+  }),
+  beforeMount() {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      this.installEvent = e;
+      this.shown = true;
+    });
+  },
+  methods: {
+    dismissPrompt() {
+      this.shown = false;
+    },
+    installPWA() {
+      this.installEvent.prompt();
+      this.installEvent.userChoice.then((choice) => {
+        this.dismissPrompt(); // Hide the prompt once the user's clicked
+        if (choice.outcome === "accepted") {
+          // Do something additional if the user chose to install
+        } else {
+          // Do something additional if the user declined
+        }
+      });
+    },
+  },
+};
+</script>
 <style>
+body {
+  font-family: sans-serif;
+}
+.app-container {
+  width: 100%;
+  height: 100vh;
+  padding-top: -30px;
+  /* padding-left: 50px;
+  padding-right: 50px; */
+  /* padding-bottom: 50px; */
+  border-color: gold;
+  border-width: 2px;
+}
+.app-container > div {
+  padding: 5vh;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+.download {
+  display: flex;
+  align-items: center;
+  position: fixed;
+  width: 10vh;
+  height: 10vh;
+  background: gold;
+  border-radius: 50%;
+  padding: 10px !important;
   text-align: center;
-  color: #2c3e50;
+  bottom: 0px;
+  right: 0;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.exit {
+  position: fixed;
+  bottom: 11vh;
+  height: 3vh;
+  width: 3vh;
+  color: white;
+  right: 1vh;
+  cursor: pointer;
 }
 </style>
